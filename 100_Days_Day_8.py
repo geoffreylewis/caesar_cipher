8f6c8c0d7f0b1e05256d4165b086c696fb9f968e
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-05-15 14:20:49 -0500

    Add files via upload
    Starting project...

---
+++ 100_Days_Day_8.py
@@ -0,0 +1,14 @@
+#################
+# Caesar Cipher #
+#################
+
+# Start of project.
+
+# print("Type \"encode\" to encrypt, or type \"decode\" to decrypt: ")
+# print("Type your message: ")
+# print("Type the shift number: ")
+# print("Here is the encoded result: ")
+# print("Type \"yes\" if you want to go again; otherwise, type \"no\": ")
+# print("Here is the decoded result: ")
+
+# End of project.
